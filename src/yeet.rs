1c8ae7dd32970577f1c127fd1f67dfe175c2b3cb
shreyaschavan228a <shreyaschavan228a@gmail.com>
2023-05-31 21:07:49 +0530

    remove old files when reading a tree

--- src/yeet.rs
+++ src/yeet.rs
@@ -103,8 +103,10 @@ pub fn write_tree(path: PathBuf) -> u64 {
 
 pub fn read_tree(hash: String, write_dir: PathBuf) {
     if !fs::try_exists(write_dir.to_owned()).expect("Unable to read dir") {
-        fs::create_dir_all(write_dir.to_owned()).expect("Unable to make dir");
+        fs::remove_dir_all(write_dir.to_owned()).expect("Unable to remove previous revision");
     }
+
+    fs::create_dir_all(write_dir.to_owned()).expect("Unable to make dir");
 
     let write_dir_name = format!("{:?}", write_dir.as_os_str());
 
